f6164febc0cf07d0a24ed31f4d82ec83361da04d
damonk@gmail.com <damonkd@gmail.com>
2019-07-03 22:31:44 -0700

    created sequilize database

---
+++ project3/models/Comments.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+    var Comments = sequelize.define("Users", {
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        
+        imageLink: {
+            type: DataTypes.STRING,
+            allowNull: true,
+      },
+
+      public: {
+        type: Boolean,
+        default: false,
+        
+      }
+
+
+    });
+
+
+Users.associate = function(models) {
+    // Associating user with budget
+    // When an user is deleted, also delete any associated budgets
+    Comments.belongsTo(models.BucketItems, {
+        foreignKey: {
+          allowNull: false
+    
+    });
+  };
+
+ 
+
+
+
+  return Users;
+
+  
+};
